f66f37c023a9f44b1e5f6403cecc1c40c155ef6e
vazgenbarfyan <vbarfyan@gmail.com>
2017-07-25 12:04:51 +0400

    Add files via upload

---
+++ src/mongo.js
@@ -0,0 +1,90 @@
+module.exports = function() {
+	return {
+		list : {},
+		setItem : function(obj){
+			this.list[obj.__id] = obj;
+		},
+		getItem : function(id){
+			return this.list[id];
+		},
+		removeItem : function(id){
+			delete this.list[id];
+		},
+		fns: {
+			$lt : function(a,b){
+				return a > b;
+			},
+			$lte : function(a,b){
+				return a >= b;
+			},
+			$gt : function(a,b){
+				return a < b;
+			},
+			$gte : function(a,b){
+				return  a <= b;
+			},
+			$eq : function(a,b){
+				return a === b;
+			},
+			$ne : function(a,b){
+				return  a !== b;
+			},
+			$in : function(a,b){
+				return (a[0]<b && b<a[1]);
+			}
+		},
+		find: function(query) {
+			const that =this;
+			const respective =[];
+			for (const key in that.list){
+				if(that.testQuery(query,that.list[key])){
+					respective.push(that.list[key]);
+				}
+			}			
+			return respective;
+		},		
+		testQuery:function (query,item){
+			const that = this;
+		 	for(const key in query){
+				const queryValue =query[key];
+				const value = item[key];
+				
+				if (typeof queryValue === "object" && typeof value === "number"){
+					for(const queryKey in queryValue) {
+					    if(queryKey.charAt(0) === '$') {
+					    	if (that.fns[queryKey]){
+								const queryExp = queryValue[queryKey];
+					    	    if(!that.fns[queryKey](queryExp, value))
+						        	return false;
+						    }
+						    else 
+						    	return false;
+     					}	
+    				}
+				}
+				if(queryValue === value){
+					return true;
+				}	
+				if(value === undefined)				
+					return false;
+				if(typeof queryValue === typeof value){
+					if(typeof value ==="object")
+						return that.testQuery(queryValue,value);
+					if(queryValue !==value)
+						return false;
+				} 				
+			}
+			return true;	
+			
+		},
+		finedOne: function(query){
+			return this.find(query)[0];
+		}
+	}
+};
+
+
+
+
+
+
--- test/test.js
+++ test/test.js
@@ -1,16 +1,47 @@
-var chai = require('chai');
-var assert = require('chai').assert;
-	// describe('Array', function() {
-	//   describe('#indexOf()', function() {
-	//     it('should return -1 when the value is not present', function() {
-	//       assert.equal(-1, [1,2,3].indexOf(4));
-	//     });
-	//   });
-	// });
-describe('Array', function() {
-  it('should start empty', function() {
-    var arr = [];
 
-    assert.equal(arr.length, 0);
+import chai from "../node_modules/chai/chai";
+import MongoStore from './mongo';
+// 'bdd' stands for "browser driven development"
+mocha.setup('bdd');
+const assert = chai.assert;
+
+describe("Testing LocalMongo",function(){
+  const store = MongoStore();
+  describe('LEVELS comparison', function() {
+    it('First level comparison', function() {
+      store.setItem({__id: 1,foo1: {},age: 44});
+      assert(store.find({age: 12}), 'Failed to locate proper age');
+    });
+    it('Second level comparison', function() {
+      store.setItem({__id: 2,zoo:{weight: 32,height: 999,age: 45}});
+      assert(store.find({zoo: {age: 45}}), 'Failed to locate proper age');
+    });
+    it('Third level comparison', function() {
+      store.setItem({__id: 3,foo: {bar: {zoo: {weight: 32,height: 999,age: 45}}}});
+      assert(store.find({foo :{bar: {zoo: {age: 45}}}}), 'Failed to locate proper age');
+    });
+    it('Deep comparison', function() {
+      store.setItem({__id:4 , foo1: {}, foo: {bar: {zoo: {weight: 32, height: 999, age: 49}}}});
+      assert(store.find({foo: {bar: {zoo: {age: 46}}}}), 'Failed to locate proper age');
+    });
   });
-});
+  describe("Opertors testing", function(){
+    it('Less than', function(){
+      assert(store.find({age: {$lt: 45}}), 'Failed to locate proper age');
+    });
+    it('Less than equal', function(){
+      assert(store.find({age: {$lte: 45}}), 'Failed to locate proper age');
+    });
+    it('Great than', function(){
+      assert(store.find({foo: {bar: {zoo: {age: {$gt: 45}}}}}), 'Failed to locate proper age');
+    });
+    it('Great than equal', function() {
+      assert(store.find({zoo: {age: {$gte: 45}}}), 'Failed to locate proper age');
+    });
+    it('Equal', function() {
+      assert(store.find({zoo: {age: {$eq: 45}}}), 'Failed to locate proper age');
+    });
+  })
+})
+
+ mocha.run();
